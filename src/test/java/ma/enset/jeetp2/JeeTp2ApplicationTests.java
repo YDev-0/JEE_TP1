21254ea718c30bd2d3e3df72561e540ca978b400
YDev-0 <78972559+YDev-0@users.noreply.github.com>
2023-04-13 20:46:20 +0000

    Initial commit

---
+++ src/test/java/ma/enset/jeetp2/JeeTp2ApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.jeetp2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeTp2ApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
